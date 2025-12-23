5227c81c685161ea69038c4a9d0ea8e4ebdc4495
priyamathur22 <priyamathur2027@gmail.com>
2025-12-23 09:53:21 +0530

    learn how to find si in java

---
+++ find_Simple_interest.java
@@ -0,0 +1,13 @@
+// Java program to calculate Simple Interest
+import java.io.*;
+class Calculator {
+    public static void main(String args[])
+    {
+        float P = 1, R = 1, T = 1;
+
+        /* Calculate simple interest */
+        float SI = (P * T * R) / 100;
+        System.out.println("Simple interest = " + SI);
+    }
+}
+// This code calculates the simple interest based on principal (P), rate of interest (R), and time (T).
