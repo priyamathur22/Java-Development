2815b426b30a89292e1cd42000505fe2b60275f3
priyamathur22 <priyamathur2027@gmail.com>
2026-01-03 14:16:56 +0530

    Basics of Arrays

--- NonPrimitive/Array.java
+++ NonPrimitive/Array.java
@@ -1,11 +1,49 @@
-package NonPrimitive;
 
-public class Main {
+import java.util.Arrays;
+public class Array{
  public static void main(String [] args) {
+    /*int age = 30;
+    int phy = 97;
+    int chem = 98;
+    int eng = 95;
 
+    int [] marks = new int [3]; //declaration and instantiation
+    marks[0] = 97;
+    marks[1]= 98;
+    marks[2]= 95;
+    System.out.println(marks[0]);
 
+    //function in array- list and sort
+    //System.out.println(marks.length);
+Arrays.sort(marks);
+System.out.println(marks[0]); //yaha pe sort hone ke baad sabse chhota element print hoga
+*/
 
+/*int[] marks = {97, 98, 95, 99, 100}; //new way to declare and instantiate an array. wee dont need to mention size of array here . and new keyword is optional 
 
-    
+//2-D array
+int [][] finalMarks = {{97,98,95},{95,95,98}};
+System.out.println(finalMarks[1][1]);*/
+/* 
+//CASTING - it is converting one data type to another data type
+double price = 100.00;
+double finalPrice = price +18.00;
+System.out.println(finalPrice);
+int p = 100;
+//int fP= p + 18.00; // yaha pe 18.00 ko int me convert kar dega compiler automatically
+//implicit casting - automatic conversion done by compiler. int small hia double se siliye java yaha pe conversion allow nahi karega. lekin small se big me conversion llow kr ddeta hia.
+    //explicit casting - manually conversion karna padta hia big se small me conversion krne ke liye. isme ham thoda bahut data loose allow karte hain.
+    int fP = p +(int)18.18; //yaha pe decimal ke baad ka part loose ho jayega
+
+System.out.println(fP);
+*/
+/* 
+//CONSTANTS - jab ham kisi variable ki value ko change nahi karna chahte to uske liye constant use karte hain. constant ko define karne ke liye 'final' keyword ka use karte hain.
+int age = 30;
+age = 31;
+float pi = 3.14f; //YE CHANGE KAR SAKTE HAIN
+final float PI = 3.14f; //constant variable
+System.out.println(PI);
+*/
  }   
 }
